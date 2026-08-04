f1c164de68528e934988ea9f8a4be7dda40c722d
MD-Saadman-Fuad <md.saadman.fuad@gmail.com>
2026-08-04 15:26:24 +0600

    Milestone 3 Module 25 Lesson 2 Complete

--- Milestone 4/Prisma Press/src/modules/subscription/subscription.controller.ts
+++ Milestone 4/Prisma Press/src/modules/subscription/subscription.controller.ts
@@ -4,19 +4,40 @@ import { subscriptionService } from "./subscription.service";
 import { sendResponse } from "../../utils/sendResponse";
 import httpStatus from "http-status";
 
-
-const createCheckoutSession = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+const createCheckoutSession = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const userId = req.user?.id;
-    const result = await subscriptionService.createCheckoutSession(userId as string);
+    const result = await subscriptionService.createCheckoutSession(
+      userId as string,
+    );
 
     sendResponse(res, {
-        success: true,
-        success_code: httpStatus.OK,
-        message: "Checkout session created successfully",
-        data: result
+      success: true,
+      success_code: httpStatus.OK,
+      message: "Checkout session created successfully",
+      data: result,
     });
-});
+  },
+);
+
+const handleWebhook = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const event = req.body as Buffer;
+    const signature = req.headers["stripe-signature"] as string;
+
+    // const result = await subscriptionService.handleWebhook(event, signature);
+    await subscriptionService.handleWebhook(event as Buffer, signature as string);
+
+    sendResponse(res, {
+      success: true,
+      success_code: httpStatus.OK,
+      message: "Webhook handled successfully",
+      data: null,
+    });
+  },
+);
 
 export const subscriptionController = {
   createCheckoutSession,
+  handleWebhook,
 };
--- Milestone 4/Prisma Press/src/modules/subscription/subscription.route.ts
+++ Milestone 4/Prisma Press/src/modules/subscription/subscription.route.ts
@@ -1,12 +1,19 @@
-import { Router } from "express";
+import express, { Router } from "express";
 import { subscriptionController } from "./subscription.controller";
 import { auth } from "../../middlewares/auth";
 import { Role } from "../../../generated/prisma/enums";
 
-
-
 const router = Router();
 
-router.post("/checkout",auth(Role.USER, Role.ADMIN, Role.AUTHOR), subscriptionController.createCheckoutSession);
+router.post(
+  "/checkout",
+  auth(Role.USER, Role.ADMIN, Role.AUTHOR),
+  subscriptionController.createCheckoutSession,
+);
 
+router.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  subscriptionController.handleWebhook,
+);
 export const subscriptionRoutes = router;
