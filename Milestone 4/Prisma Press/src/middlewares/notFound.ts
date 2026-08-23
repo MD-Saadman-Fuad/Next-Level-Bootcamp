3bd8383f52b38dc02b598bf404867d1c4e1ac842
MD-Saadman-Fuad <md.saadman.fuad@gmail.com>
2026-08-24 01:10:41 +0600

    Milestone 5 Module 27 Lesson 5 Done

--- Milestone 4/Prisma Press/src/middlewares/notFound.ts
+++ Milestone 4/Prisma Press/src/middlewares/notFound.ts
@@ -8,6 +8,6 @@ export const notFound = (req: Request, res: Response) => {
     path: req.originalUrl,
     date: Date(),
     message: "Route Not Found",
-    data: new Date().toISOString(),
+    data: null,
   });
 };
---
+++ Milestone 5/nextjs/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      Hello Next Js
+    </div>
+
+  );
+}
